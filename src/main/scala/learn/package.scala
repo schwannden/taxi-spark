284a73812c1dbb2b1993cc8a3ce92987891ae5d5
schwannden <schwanndn@gmail.com>
2017-03-20 14:18:50 +0800

    put most analysis in lean package

--- src/main/scala/learn/package.scala
+++ src/main/scala/learn/package.scala
@@ -5,8 +5,8 @@ import org.apache.spark.rdd.RDD
 import preprocess._
 
 package object learn {
-  def gmm(data: RDD[Vector], k: Int, nIter: Int): GaussianMixtureModel = {
-    new GaussianMixture().setK(k).setMaxIterations(nIter).run(data)
+  def gmm(data: RDD[Vector], k: Int = 2): GaussianMixtureModel = {
+    new GaussianMixture().setK(k).run(data)
   }
 
   def printGmm(gmmResult: GaussianMixtureModel, means: Array[Double] = Array(0, 0), factor: Double = 1.0): Unit = {
@@ -21,34 +21,40 @@ package object learn {
     }
   }
 
-  def runGmm(data: TaxiData, sampleRatio: Double = 0.01): Unit = {
-    val pickRaw = data.pickLocation.sample(false, sampleRatio).cache
-    val dropRaw = data.dropLocation.sample(false, sampleRatio).cache
-    val factor = 1000
-    val pickMean = Statistics.colStats(pickRaw).mean.toArray
-    val dropMean = Statistics.colStats(dropRaw).mean.toArray
-    val pick = normalize(pickRaw, pickMean, factor)
-    val drop = normalize(dropRaw, dropMean, factor)
+  def runGmm(locations: RDD[Vector], k: Int = 2, normalized: Boolean = false): GaussianMixtureModel = {
     val t1 = System.currentTimeMillis
-    val (k, nIter) = (10, 90)
-    val gmmPickup = gmm(pick, k, nIter)
+    val res = if (normalized) {
+      val factor = 1000
+      val mean = Statistics.colStats(locations).mean.toArray
+      val locationsNormalized = normalize(locations, mean, factor)
+      gmm(locationsNormalized, k)
+    } else {
+      gmm(locations, k)
+    }
     val t2 = System.currentTimeMillis
-    val gmmDropoff = gmm(drop, k, nIter)
-    val t3 = System.currentTimeMillis
-    printGmm(gmmPickup, pickMean, factor)
-    printGmm(gmmDropoff, pickMean, factor)
-    println(t2 - t1)
-    println(t3 - t2)
-  }
-
-  def peakHour(hours: RDD[Int]) = {
-    hours.groupBy(identity).mapValues(_.size)
+    println("Running Time: " + (t2 - t1)/60000.0 + " minutes")
+    res
   }
 
   def distanceAnalysis(data: TaxiData): Unit = {
     val ds = data.distance
     val summary = Statistics.colStats(ds)
     print(summary.mean)
+  }
+
+  def expCluster(data: TaxiData, k: Int, normalized: Boolean = false): Unit = {
+    val res1 = runGmm(data.pickLocation, k = k, normalized = normalized)
+    printGmm(res1)
+    val res2 = runGmm(data.dropLocation, k = k, normalized = normalized)
+    printGmm(res2)
+  }
+
+  def expHourCount(data: TaxiData): Unit = {
+    val pickHour = data.pickHour.groupBy(v => v(0)).mapValues(_.size)
+    print(pickHour.collect.sortWith((x, y) => x._2 > y._2))
+
+    val dropHour = data.dropHour.groupBy(v => v(0)).mapValues(_.size)
+    print(dropHour.collect.sortWith((x, y) => x._2 > y._2))
   }
 
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,6 +3,7 @@ package main
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.mllib.stat.Statistics
 
 object TaxiCluster {
   val conf = new SparkConf().setAppName("taxi")
@@ -13,12 +14,16 @@ object TaxiCluster {
       .config("spark.some.config.option", "some-value")
       .getOrCreate()
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     import preprocess._
     import learn._
     val file = "data/taxi201601.csv"
     val data = new TaxiData(getData(sc, file))
-    runGmm(data)
-    runGmm(data.sanitize)
+    val dataSanitized = data.sanitize
+    expCluster(data, 2)
+    expCluster(dataSanitized, 2)
+    expCluster(dataSanitized, 10)
+    expCluster(dataSanitized, 10, true)
+
   }
 }
